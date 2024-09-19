fb13b509da4af6b7445603be3e21f7af27f3607d
Mamoudou12 <bmamoudou463@gmail.com>
2024-09-19 00:31:32 +0000

    implement orderModal file

---
+++ src/views/orders/OrderModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal fade" id="orderModal" tabindex="-1" aria-labelledby="orderModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="orderModalLabel">Add Order</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <!-- Formulaire d'ajout de commande -->
+            <OrderForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import OrderForm from './OrderForm.vue';
+  </script>
+  
+  <style scoped>
+  .modal-lg {
+    max-width: 100%;
+    margin-top: 0;
+  }
+  </style>
+  
